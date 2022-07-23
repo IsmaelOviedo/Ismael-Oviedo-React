ea00bb6633cc7c4e4637d24cff70c54c3d506e89
ismael oviedo <ismael.nfsmw@gmail.com>
2022-07-23 15:48:49 -0300

    asyncMock

---
+++ mi-app/src/asyncMock.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        id: '1',
+        name: 'Vestido Azul',
+        price: 5000,
+        category: 'Vestimenta',
+        img: 'imagenes/vestido-azul.jpg',
+        stock: 5,
+        description: 'Vestido de moda',
+    },
+
+    {
+        id: '2',
+        name: 'Falda Negra',
+        price: 500,
+        category: 'Vestimenta',
+        img: 'imagenes/falda-negra.jpg',
+        stock: 10,
+        description: 'Outfit',
+    },
+
+    {
+        id: '3',
+        name: 'Vestido Verde',
+        price: 4000,
+        category: 'Vestimenta',
+        img: 'imagenes/vestido-verde.png',
+        stock: 2,
+        description: 'Elegancia',  
+    }
+]
