56532be63aaeea98e7a42abde29c4786a515cd9c
ismael oviedo <ismael.nfsmw@gmail.com>
2022-08-10 22:23:11 -0300

    CartContext

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -4,25 +4,26 @@ import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartContextProvider } from './Context/CartContext'
+import { UserContextProvider} from './Context/UserContext'
 
 
 function App() {
   return (
     <div className="App">
-     
+  <UserContextProvider>
+    <CartContextProvider>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path='/inicio' element={<ItemListContainer greeting="Alaina Store" />}/>
+          <Route exact path='/inicio' element={<ItemListContainer greeting="AlainaStore" />}/>
           <Route exact path='/productos' element={<ItemListContainer greeting="Estamos filtrando" />} />
           <Route exact path='/detail/:productId' element={<ItemDetailContainer />} />
+          <Route path='/cart' element={<h1>Cart</h1>} />
         </Routes>
       </BrowserRouter>
-
-
-        
-
-      
+    </CartContextProvider>
+  </UserContextProvider>
     </div>
   );
 }
