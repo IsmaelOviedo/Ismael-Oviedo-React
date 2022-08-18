4d28f58ed4596272f7414ce7321950e63fd1bfa1
ismael oviedo <ismael.nfsmw@gmail.com>
2022-08-17 22:16:43 -0300

    service

---
+++ mi-app/src/services/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrrZ7zdlUkmkVEsOH-BxUCqf6ahs6W3Ms",
+  authDomain: "proyecto-mi-app.firebaseapp.com",
+  projectId: "proyecto-mi-app",
+  storageBucket: "proyecto-mi-app.appspot.com",
+  messagingSenderId: "640874308866",
+  appId: "1:640874308866:web:410d81c1e4658e0e0ece1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
